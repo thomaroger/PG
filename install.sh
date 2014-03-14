d7a56694d63ba154406409a31691c2c3480626bb
Thomas ROGER <thomaroger@gmail.com>
2014-03-14 23:37:53 +0100

    add plugin translate in subtemplate

--- install.sh
+++ install.sh
@@ -4,9 +4,12 @@ if [ $1 ]
 then
     ENV="$1"
 fi
+git pull origin develop
+php composer.phar update
 php vendor/doctrine/doctrine-module/bin/doctrine-module.php orm:schema-tool:drop --force
 php vendor/doctrine/doctrine-module/bin/doctrine-module.php orm:schema-tool:create
 php vendor/doctrine/doctrine-module/bin/doctrine-module.php data-fixture:import --append
 php vendor/doctrine/doctrine-module/bin/doctrine-module.php orm:schema-tool:update --force
 php public/index.php assetic setup
 php public/index.php assetic build
+chmod -R 777 *
